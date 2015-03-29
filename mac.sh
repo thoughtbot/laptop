992d75de0d7f242954db72f0e2a12eecca3b9ed7
Ian Yamey <ian@policygenius.com>
2015-03-29 14:05:24 -0400

    Inline setup_the_dock.sh into mac.sh

--- mac.sh
+++ mac.sh
@@ -284,7 +284,32 @@ git config --global alias.squash "commit --squash"
 git config --global alias.unstage "reset HEAD"
 git config --global alias.rum "rebase master@{u}"
 
-sh setup_the_dock.sh
+defaults write com.apple.Dock autohide 1
+
+dockutil --remove "Launchpad" --no-restart
+dockutil --remove "Safari" --no-restart
+dockutil --remove "Mail" --no-restart
+dockutil --remove "Contacts" --no-restart
+dockutil --remove "Calendar" --no-restart
+dockutil --remove "Notes" --no-restart
+dockutil --remove "Map" --no-restart
+dockutil --remove "Facetime" --no-restart
+dockutil --remove "iPhoto" --no-restart
+dockutil --remove "Pages" --no-restart
+dockutil --remove "Numbers" --no-restart
+dockutil --remove "Keynote" --no-restart
+dockutil --remove "iTunes" --no-restart
+dockutil --remove "iBooks" --no-restart
+
+dockutil --add /Applications/Google\ Chrome.app --no-restart
+dockutil --add ~/Applications/iTerm.app/ --no-restart
+dockutil --add ~/Applications/Slack.app/ --no-restart
+dockutil --add ~/Applications/Sublime Text 2.app/ --no-restart
+dockutil --add ~/Applications/MacVim.app/ --no-restart
+dockutil --add ~/Applications/Screenhero.app/ --no-restart
+dockutil --add ~/Applications/SourceTree.app/ --no-restart
+
+/usr/bin/killall -HUP Dock >/dev/null 2>&1
 
 if [ -f "$HOME/.laptop.local" ]; then
   . "$HOME/.laptop.local"
