38e99dc68680d0c7ea36a651d3f30ab26e40e3f7
Ian Yamey <ian@policygenius.com>
2015-03-29 13:12:30 -0400

    Inline setup_git_aliases.sh into mac.sh

--- mac.sh
+++ mac.sh
@@ -231,7 +231,27 @@ defaults write NSGlobalDomain InitialKeyRepeat -int 12
 
 sh vim_config.sh
 sh install_pivotal_git_scripts.sh
-sh setup_git_aliases.sh
+
+# Setup git aliases
+git config --global alias.st status
+git config --global alias.di diff
+git config --global alias.co checkout
+git config --global alias.ci commit
+git config --global alias.br branch
+git config --global alias.sta stash
+git config --global alias.llog "log --date=local"
+git config --global alias.flog "log --pretty=fuller --decorate"
+git config --global alias.lg "log --graph --pretty=format:\"%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset\" --abbrev-commit --date=relative"
+git config --global alias.lol "log --graph --decorate --oneline"
+git config --global alias.lola "log --graph --decorate --oneline --all"
+git config --global alias.blog "log origin/master... --left-right"
+git config --global alias.ds "diff --staged"
+git config --global alias.fixup "commit --fixup"
+git config --global alias.squash "commit --squash"
+git config --global alias.unstage "reset HEAD"
+git config --global alias.rum "rebase master@{u}"
+
+
 sh setup_the_dock.sh
 sh install_shiftit.sh
 
